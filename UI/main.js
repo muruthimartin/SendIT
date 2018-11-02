807f1fca11733b21a0b775c580ce8758dc736be4
martin muruthi <muruthimartin@gmail.com>
2018-11-02 14:40:06 -0400

    [finishes #161581274] Built a cancel order page

---
+++ UI/main.js
@@ -0,0 +1,7 @@
+function cancel(){
+    prompt("Are you sure you want to cancel the current order? ");
+}
+function changeDestination(){
+    prompt("Are you sure you want to change the destination of the current order?")
+
+}
